0f58abe8692847ce5df76c9214576cd111795f1d
Jeswin-8801 <jeswin.santosh@outlook.com>
2024-08-19 21:35:24 +0530

    Create Spring boot project and add postgres to docker compose

---
+++ backend/code-node/src/main/java/com/jeswin8801/code_node/CodeNodeApplication.java
@@ -0,0 +1,13 @@
+package com.jeswin8801.code_node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNodeApplication.class, args);
+	}
+
+}
---
+++ backend/code-node/src/test/java/com/jeswin8801/code_node/CodeNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeswin8801.code_node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
