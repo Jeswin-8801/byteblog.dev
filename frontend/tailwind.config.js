9324eda90df4160884b08f3e86f717f917ee7e2c
Jeswin-8801 <jeswin.santosh@outlook.com>
2024-09-04 15:22:08 +0530

    set up home, login and sign up pages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+// const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}", // add this line
   ],
   theme: {
-    extend: {},
+    // fontFamily: {
+    //   'sans': ['Helvetica', ...defaultTheme.fontFamily.sans],
+    // },
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
 
